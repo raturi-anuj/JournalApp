dd979d0d5240e8ddf3486bdb108f7eb739fa8b0d
Anuj Raturi <anujraturi802@gmail.com>
2024-10-15 18:42:54 +0530

    added transaction manager

--- src/main/java/net/personalproject/journalApp/service/JournalEntryService.java
+++ src/main/java/net/personalproject/journalApp/service/JournalEntryService.java
@@ -6,6 +6,7 @@ import net.personalproject.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,12 +21,19 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName) {
-        User user = userService.findByUserName(userName);
-        journalEntry.setDate(LocalDateTime.now());
-        JournalEntry saved = journalEntryRepository.save(journalEntry);
-        user.getJournalEntries().add(saved);
-        userService.saveEntry(user);
+        try {
+            User user = userService.findByUserName(userName);
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry saved = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(saved);
+            user.setUserName(null);
+            userService.saveEntry(user);
+        }catch (Exception e){
+            System.out.println(e);
+            throw new RuntimeException("An error occurred while saving the entry.", e);
+        }
     }
 
     public void saveEntry(JournalEntry journalEntry) {
